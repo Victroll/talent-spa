8fb9ddffa91227072200899f5b11bb8b4ec1bd84
vgutierrez <vgutierrez@ctisoluciones.com>
2018-02-22 12:54:45 +0100

    Added the gsap/Draggable module. Talents can be dragged

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,18 @@ import * as types from "../constants/actions";
 
 export default function(state, action) {
     switch (action.type) {
-        case types.ADD_ONE:
-            return {...state, ['value']: state.value + 1}
-        case types.SUBSTRACT_ONE:
-            return {...state, ['value']: state.value - 1}   
-        case types.DOUBLE_VALUE:
-            return {...state, ['value']: state.value * 2}
+        case types.ADD_NEW_TALENT:
+            // Check if the component exists in the array
+            if (!state.talents.filter((talent) => {
+                    return talent.props.id === action.talent.props.id;
+                }).length) {
+                    action.talent.showImage();
+                    return {...state, 
+                        ['talents']: state.talents.push(action.talent)
+                    };
+            } else {
+                return state;
+            }
         default:
             return state;
     }
